8217ed608a239064da53645ba36fd8dc0d499590
juggato06 <vincentlow2006@gmail.com>
2025-11-04 20:11:30 +0800

    add sounds

--- CGHC/Assets/Scripts/Environment/AudioManager.cs
+++ CGHC/Assets/Scripts/Environment/AudioManager.cs
@@ -1,51 +1,85 @@
 using UnityEngine;
+using System.Collections.Generic; // We need this for the Dictionary
 
-// This line automatically adds an AudioSource when you add this script
-[RequireComponent(typeof(AudioSource))]
+// This struct is a helper. It lets us create a nice-looking list
+// of named sound effects in the Inspector.
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+}
+
 public class AudioManager : MonoBehaviour
 {
     // This 'Instance' part is the "Singleton" pattern.
-    // It lets us keep track of the one and only AudioManager.
     public static AudioManager Instance { get; private set; }
 
-    private AudioSource musicSource;
+    [Header("Audio Sources")]
+    [Tooltip("The AudioSource component for playing music.")]
+    public AudioSource musicSource;
+    [Tooltip("The AudioSource component for playing sound effects.")]
+    public AudioSource sfxSource;
 
-    [Header("Music Clip")]
+    [Header("Audio Clips")]
     [Tooltip("The background music you want to play.")]
     public AudioClip backgroundMusic;
 
+    [Tooltip("The list of all sound effects for the game.")]
+    public Sound[] sfxList;
+
+    // A Dictionary is a high-speed way to look up clips by their name.
+    private Dictionary<string, AudioClip> sfxMap;
+
     void Awake()
     {
-        // --- This is the core logic ---
-        
+        // --- Singleton Logic ---
         if (Instance == null)
         {
-            // If this is the FIRST time, set this object as the Instance
             Instance = this;
-            
-            // Tell Unity: "Don't destroy this GameObject when a new scene loads."
             DontDestroyOnLoad(gameObject);
         }
         else
         {
-            // If an Instance ALREADY exists (from a previous scene),
-            // then destroy this new, duplicate object.
             Destroy(gameObject);
-            return; // Stop running the rest of Awake()
+            return;
         }
-        
-        // --- Setup the AudioSource ---
-        musicSource = GetComponent<AudioSource>();
-        musicSource.clip = backgroundMusic;
-        musicSource.loop = true; // Make sure the music loops
-        
-        // Optional: If you want 2D music (not 3D positional)
-        musicSource.spatialBlend = 0f; 
 
-        // Play the music
-        if (backgroundMusic != null)
+        // --- Build the SFX Dictionary ---
+        // We turn the list into a fast-lookup Dictionary
+        // so we can call sounds by their name string.
+        sfxMap = new Dictionary<string, AudioClip>();
+        foreach (Sound s in sfxList)
         {
+            sfxMap[s.name] = s.clip;
+        }
+
+        // --- Setup and Play Music ---
+        if (musicSource != null && backgroundMusic != null)
+        {
+            musicSource.clip = backgroundMusic;
+            musicSource.loop = true;
             musicSource.Play();
+        }
+    }
+
+    /// <summary>
+    /// Plays a sound effect one time.
+    /// </summary>
+    /// <param name="soundName">The name of the sound from the Sfx List.</param>
+    public void PlaySFX(string soundName)
+    {
+        // Find the clip from the dictionary
+        if (sfxMap.TryGetValue(soundName, out AudioClip clip))
+        {
+            // Play it as a "one-shot" sound. This allows multiple
+            // sounds to play at once without cutting each other off.
+            sfxSource.PlayOneShot(clip);
+        }
+        else
+        {
+            // A small warning if you try to play a sound that doesn't exist
+            Debug.LogWarning("AudioManager: SFX not found: " + soundName);
         }
     }
 }
